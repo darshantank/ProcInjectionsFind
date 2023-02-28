331d996fdbd134479d3ea4ccae3c3e911dab83f2
Darshan Tank <dmtank@gmail.com>
2023-02-28 15:34:32 +0530

    Update ProcInjectionsFind.py

--- ProcInjectionsFind.py
+++ ProcInjectionsFind.py
@@ -6,6 +6,10 @@
 
 # Plugin Name: ProcInjectionsFind
 
+# ProcInjectionsFind is a Volatility plugin to detect varied types of process injections. It displays attributes for each injected memory region (VAD).
+
+# ProcInjectionsFind allow a security analyst to get the process related information and spot any process anamoly without having to run multiple plugins.
+
 # Description: A volatility plugin to detect following types of process injections. 
 	# 1. Classic DLL Injection Via CreateRemoteThread and LoadLibrary
 	# 2. Simple Thread Injection Using CreateRemoteThread
@@ -15,22 +19,17 @@
 	# 6. Thread Execution Hijacking
 	# 7. APC Injection
 	# 8. AtomBombing
-
-# GetInjectedThread is a Volatility plugin to detect varied types of process injections. It displays attributes for each injected memory region (VAD).
-
-# This plugin should allow a security analyst to get the process related information and spot any process anamoly without having to run multiple plugins.
-
 '''
-### Running the Plugin
+Running the Plugin
 
-Copy the plugin to volatility/plugins directory
+Copy the plugin to volatility/plugins directory.
 
-Run the plugin against malware-infected windows memory image as shown below
+Run the plugin against malware-infected windows memory image as shown below.
 
 $ python vol.py -f win7-Guest-clone.mem --profile=Win7SP1x64 procinjectionsfind
 $ python vol.py -f win10-Guest-clone.mem --profile=Win10x64_14393 procinjectionsfind
 
-Run the plugin against memory of a live VM as shown below
+Run the plugin against memory of a live VM as shown below.
 
 $ python vol.py -l vmi://win7_Guest --profile=Win7SP1x64 procinjectionsfind
 $ python vol.py -l vmi://win10_Guest --profile=Win10x64_14393 procinjectionsfind
@@ -59,8 +58,6 @@ from volatility.plugins.taskmods import PSList
 # 'VadImageMap'
 IMAGE_FILE_TYPE = 2
 start_time = time.time()
-
-# if vad.Start <= thread.Win32StartAddress <= vad.End and vad.u.VadFlags.VadType.v() != IMAGE_FILE_TYPE and vad.u.VadFlags.MemCommit.v() == 1:
 
 class ProcInjectionsFind(vadinfo.VADDump):
 	"""Print attributes for each injected memory region (VAD)"""
@@ -104,7 +101,6 @@ class ProcInjectionsFind(vadinfo.VADDump):
 
 	def parse_process(self, proc_object):
 
-		# --- 100 line ---
 		process = proc_object
 		process_name = process.ImageFileName
 		process_pid = process.UniqueProcessId
@@ -121,7 +117,6 @@ class ProcInjectionsFind(vadinfo.VADDump):
 
 		return output
 
-	# --- 100 lines ---
 	def parse_thread(self, thread_object):
 	
 		thread = thread_object
@@ -133,6 +128,7 @@ class ProcInjectionsFind(vadinfo.VADDump):
 		thread_CrossThreadFlags = thread.CrossThreadFlags
 
 		# Check if the thread is terminated
+		
 		if thread.Terminated != 0:
         		thread_state = 'Terminated'
         	else:
@@ -204,7 +200,6 @@ class ProcInjectionsFind(vadinfo.VADDump):
 					      for o, i, h in \
 					      malfind.Disassemble(content, vad.Start)])
 		else:
-			# --- 200 line ---
         		disassemble_code = "\t** Couldn't read memory\n"
 
 		return disassemble_code
@@ -228,18 +223,6 @@ class ProcInjectionsFind(vadinfo.VADDump):
 			disassemble_code += "\n\t".join(["{0:#010x} {1:<16} {2}".format(o, h, i)  \
 					      for o, i, h in \
 					      malfind.Disassemble(content, int(thread_entry_point))])
-
-			'''
-			mode = address_space.profile.metadata.get('memory_model')
-                	if mode == '64bit':
-                    		mode = distorm3.Decode64Bits
-                	else:
-                    		mode = distorm3.Decode32Bits
-                	disassemble_code += "\n\t".join(["{0:<#010x} {1:<16} {2}".format(o, h, i) \
-                                              for o, _size, i, h in \
-                                              distorm3.DecodeGenerator(int(thread_entry_point), content, mode)])
-			'''
-
 		else:
         		disassemble_code = "\t** Couldn't read memory\n"
 
@@ -279,17 +262,14 @@ class ProcInjectionsFind(vadinfo.VADDump):
 	def injection_filter_thread(self, vad):
 	
 		# check-1 --> Find unmapped threads in process memory
-		# Unmapped thread: A thread in the process that is mapped to a VAD without a file 					  object.
+		# Unmapped thread: A thread in the process that is mapped to a VAD without a file object.
 		if hasattr(vad,"ControlArea") == False:
-			#print 1
 			return True
 
-		# check-2 --> A thread in the process that is mapped to a VAD with a file object, but 				     the memory is committed and the type of file is not an image file.
+		# check-2 --> A thread in the process that is mapped to a VAD with a file object, but the memory is committed and the type of file is not an image file.
 		if hasattr(vad.VadFlags,"MemCommit") == True:
 			if vad.VadFlags.MemCommit.v() == 1 and vad.VadFlags.VadType.v() != IMAGE_FILE_TYPE:
-				#print 2
 				return True
-		
 			return False
 
 	def injection_filter_vad(self, process_space, vad):
@@ -297,7 +277,7 @@ class ProcInjectionsFind(vadinfo.VADDump):
 		protection = vadinfo.PROTECT_FLAGS.get(vad.VadFlags.Protection.v(), "")
 		vad_type = vadinfo.MI_VAD_TYPE.get(vad.VadFlags.VadType.v(), "")
 
-		# check-3 --> Found signature ("MZ") with no ControlArea and PAGE_EXECUTE_READWRITE 			permission in VAD
+		# check-3 --> Found signature ("MZ") with no ControlArea and PAGE_EXECUTE_READWRITE permission in VAD
 		''' 
 		data = process_space.read(vad.Start, 1024)
 		if data:
@@ -308,7 +288,6 @@ class ProcInjectionsFind(vadinfo.VADDump):
 					return True
 		'''
 		# check-4 --> Detects Hollow Process Injection 
-
 		if vad.Tag != "VadS":
 			control_area = vad.ControlArea
 			if not control_area:
@@ -319,12 +298,10 @@ class ProcInjectionsFind(vadinfo.VADDump):
             				return True
 		
 		# check-5 --> Detects APC Injection & AtomBombing 
-
 		if hasattr(vad.VadFlags,"CommitCharge") == True:
 			if vad.VadFlags.PrivateMemory == 1 and vad.Tag == "VadS" and protection == "PAGE_EXECUTE_READWRITE" and vad_type == "VadNone" and vad.VadFlags.CommitCharge == 1 and vad.VadFlags.MemCommit == 1:
 				#print 5
             			return True
-
 		return False
 
 	def calculate(self):
@@ -363,16 +340,7 @@ class ProcInjectionsFind(vadinfo.VADDump):
 							if self.check_if_thread_is_suspended(thread) == True:
 								print "Found suspended thread"
 								yield (proc, vad)
-						'''
-						else:
-							print "Found different exe file object from the loaded process's image file"
-							yield (proc, vad)
-						'''
-						#print ntpath.basename(str(self.get_vad_file_object(vad)))
-						#print proc.ImageFileName
-
-		
-		#for proc in psdata:
+								
 			process_space = proc.get_process_address_space()
 			for vad in proc.VadRoot.traverse():
 				if self.injection_filter_vad(process_space, vad) == True:
@@ -396,11 +364,7 @@ class ProcInjectionsFind(vadinfo.VADDump):
 			outfd.write("------------------------------------------------------------------------------------" + '\n')
 			outfd.write(self.parse_process(proc))
 			outfd.write("------------------------------------------------------------------------------------" + '\n')
-			'''
-			# Print out thread information
-			outfd.write("Thread Info:\n")
-			outfd.write(self.parse_thread(thread))
-			'''
+			
 			# Print out thread's mapped VAD information
 			outfd.write("VAD Info:\n")
 			outfd.write(self.parse_vad(vad))
@@ -409,35 +373,7 @@ class ProcInjectionsFind(vadinfo.VADDump):
 			outfd.write("Disassembly Info:\n")
 			outfd.write(self.disassemble_vad(address_space, vad))
 			outfd.write("\n")
-			'''
-			# Dump the vad data
-			proc_pid = proc.UniqueProcessId
-			thread_id = thread.Cid.UniqueThread.v()
-			thread_entry_point = thread.Win32StartAddress
 
-                        if dump_dir:
-                            filename = "Process.{0}.Vad.{1:#x}.dmp".format(proc.UniqueProcessId, vad.Start)
-                            full_path = os.path.join(dump_dir, filename)
-                            self.dump_vad(full_path, vad, address_space)
-			'''
-
-			'''
-			# Print out Disassembly	of thread's entry point
-			outfd.write("Disassembly Info:\n")
-			outfd.write(self.disassemble_thread(address_space, thread))
-			outfd.write("\n")
-			
-			# Dump thread's entry point data
-			proc_pid = proc.UniqueProcessId
-			thread_id = thread.Cid.UniqueThread.v()
-			thread_entry_point = thread.Win32StartAddress
-
-                        if dump_dir:
-                            filename = "Process.{0}.Thread.{1}.entrypoint.{2:#x}.dmp".format(proc_pid, thread_id, thread_entry_point)
-                            full_path = os.path.join(dump_dir, filename)
-                            self.dump_vad(full_path, vad, address_space)
-			'''
-			
 		end_time = time.time()
 		print "\nElapsed Wall-Clock Time: " + str(end_time - start_time) + " seconds" + "\n"
 
